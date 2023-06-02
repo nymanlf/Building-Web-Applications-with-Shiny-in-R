13355173cbf93d4685077324843af27f3f291abd
nymanlf <nymanlf@mathr.math.appstate.edu>
2023-06-02 18:04:02 -0400

    updated 6/2

--- C2AddSelectInput.R
+++ C2AddSelectInput.R
@@ -5,24 +5,18 @@ library(babynames)
 data(babynames, package = "babynames")
 ui <- fluidPage(
   titlePanel("What's in a Name?"),
-  # CODE BELOW: Add select input named "sex" to choose between "M" and "F"
   selectInput('sex', 'Select Sex', choices = c("F", "M")),
-  # Add plot output to display top 10 most popular names
   plotOutput('plot_top_10_names')
 )
 
 server <- function(input, output, session){
-  # Render plot of top 10 most popular names
   output$plot_top_10_names <- renderPlot({
-    # Get top 10 names by sex and year
     top_10_names <- babynames %>% 
-      # MODIFY CODE BELOW: Filter for the selected sex
-      filter(sex == "F") %>% 
+      filter(sex == input$sex) %>% 
       filter(year == 1900) %>% 
       slice_max(prop, n = 10)
-    # Plot top 10 names by sex and year
     ggplot(top_10_names, aes(x = name, y = prop)) +
-      geom_col(fill = "#263e63")
+      geom_col(fill = ifelse(input$sex == "F", "pink", "blue"))
   })
 }
 
